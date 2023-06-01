e28181f209e835e2fdfe80f6a3edde33823f1e02
manh <manhtdph19414@fpt.edu.vn>
2023-06-01 11:09:51 +0700

    manhlaithem

--- QLDuAn/QLDuAn/web-ql-exp/routes/khachsanpham.js
+++ QLDuAn/QLDuAn/web-ql-exp/routes/khachsanpham.js
@@ -3,14 +3,18 @@ var multer = require('multer');
 var router = express.Router();
 var spCtrl = require('../controllers/khachsanpham.controller');
 var upload = multer({ dest : './tmp'});
+var check_login = require('../middlewares/check_login');
+router.use ( (req,res,next)=>{
+    console.log('middleware');
+    next();
+});
+router.get('/home',check_login.yeu_cau_login,spCtrl.home );
+router.get('/tang',check_login.yeu_cau_login,spCtrl.giatientang);
+router.get('/giam',check_login.yeu_cau_login,spCtrl.giatiengiam);
+router.get('/home/:idloai',check_login.yeu_cau_login,spCtrl.loctheoLoai);
 
-router.get('/home',spCtrl.home );
-router.get('/tang',spCtrl.giatientang);
-router.get('/giam',spCtrl.giatiengiam);
-router.get('/home/:idloai',spCtrl.loctheoLoai);
 
-
-router.get('/chitietsanpham/:idsp',spCtrl.chitietsanpham );
-router.post('/chitietsanpham/:idsp',spCtrl.chitietsanpham );
+router.get('/chitietsanpham/:idsp',check_login.yeu_cau_login,spCtrl.chitietsanpham );
+router.post('/chitietsanpham/:idsp',check_login.yeu_cau_login,spCtrl.chitietsanpham );
 
 module.exports = router;
--- QLDuAn/QLDuAn/web-ql-exp/routes/sanpham.js
+++ QLDuAn/QLDuAn/web-ql-exp/routes/sanpham.js
@@ -3,35 +3,39 @@ var multer = require('multer');
 var router = express.Router();
 var spCtrl = require('../controllers/sanpham.controller');
 var upload = multer({ dest : './tmp'});
+var check_login = require('../middlewares/check_login');
+router.use ( (req,res,next)=>{
+    console.log('middleware');
+    next();
+});
+
+router.get('/home',check_login.yeu_cau_login,spCtrl.home );
+router.post('/home',check_login.yeu_cau_login,spCtrl.home );
+router.get('/tang',check_login.yeu_cau_login,spCtrl.giatientang);
+router.get('/giam',check_login.yeu_cau_login,spCtrl.giatiengiam);
+
+router.get('/addsp',check_login.yeu_cau_login,spCtrl.addsp);
+router.post('/addsp',check_login.yeu_cau_login,upload.single('image'),spCtrl.addsp);
+
+router.get('/home/:idloai',check_login.yeu_cau_login,spCtrl.loctheoLoai);
 
 
-router.get('/home',spCtrl.home );
-router.post('/home',spCtrl.home );
-router.get('/tang',spCtrl.giatientang);
-router.get('/giam',spCtrl.giatiengiam);
 
-router.get('/addsp',spCtrl.addsp);
-router.post('/addsp',upload.single('image'),spCtrl.addsp);
+router.get('/editSP/:idsp',check_login.yeu_cau_login,spCtrl.editsp );
+router.post('/editSP/:idsp',check_login.yeu_cau_login,upload.single('image'),spCtrl.editsp );
 
-router.get('/home/:idloai',spCtrl.loctheoLoai);
-
+router.get('/deleteSP/:idsp',check_login.yeu_cau_login,spCtrl.deleteSP);
+router.delete('/deleteSP/:idsp',check_login.yeu_cau_login,spCtrl.deleteSP);
+router.get('/xacnhandeleteSP/:idsp',check_login.yeu_cau_login,spCtrl.xacnhandeleteSP);
+router.delete('/xacnhandeleteSP/:idsp',check_login.yeu_cau_login,spCtrl.xacnhandeleteSP);
 
 
-router.get('/editSP/:idsp',spCtrl.editsp );
-router.post('/editSP/:idsp',upload.single('image'),spCtrl.editsp );
-
-router.get('/deleteSP/:idsp',spCtrl.deleteSP);
-router.delete('/deleteSP/:idsp',spCtrl.deleteSP);
-router.get('/xacnhandeleteSP/:idsp',spCtrl.xacnhandeleteSP);
-router.delete('/xacnhandeleteSP/:idsp',spCtrl.xacnhandeleteSP);
-
-
-router.get('/editLoai/:idloai',spCtrl.editloai );
-router.post('/editLoai/:idloai',spCtrl.editloai );
-router.get('/deleteLoai/:idloai',spCtrl.deleteLoai);
-router.delete('/deleteLoai/:idloai',spCtrl.deleteLoai);
-router.get('/xacnhandeleteLoai/:idloai',spCtrl.xacnhandeleteLoai);
-router.delete('/xacnhandeleteLoai/:idloai',spCtrl.xacnhandeleteLoai);
+router.get('/editLoai/:idloai',check_login.yeu_cau_login,spCtrl.editloai );
+router.post('/editLoai/:idloai',check_login.yeu_cau_login,spCtrl.editloai );
+router.get('/deleteLoai/:idloai',check_login.yeu_cau_login,spCtrl.deleteLoai);
+router.delete('/deleteLoai/:idloai',check_login.yeu_cau_login,spCtrl.deleteLoai);
+router.get('/xacnhandeleteLoai/:idloai',check_login.yeu_cau_login,spCtrl.xacnhandeleteLoai);
+router.delete('/xacnhandeleteLoai/:idloai',check_login.yeu_cau_login,spCtrl.xacnhandeleteLoai);
 
 
 module.exports = router;
